532397141045d4d5736c7dff76397a0fd278423c
Vishnuvardhan14 <vishnu14aug@gmail.com>
2022-08-08 09:30:46 +0530

    demo of tuples

---
+++ activities/src/bin/demo3.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let coord=(2,3);
+    println!("{:?},{:?}",coord.0,coord.1);
+    let (x,y)=(2,3);
+    println!("{:?},{:?}",x,y);
+    let (name,age)=("Emma",20);
+    let favourites =("red",14,"TX","pizza","TV SHOW","home");
+    let state=favourites.2;
+    let place=favourites.5;
+
+}
