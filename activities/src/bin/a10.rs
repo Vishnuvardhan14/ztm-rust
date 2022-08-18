6926442b17408b56f7651c7c978b491a86fcd88d
Vishnuvardhan14 <vishnu14aug@gmail.com>
2022-08-18 10:03:45 +0530

    Working with expressions

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -9,7 +9,17 @@
 //   an if..else expression to store whether the value
 //   is > 100 or <= 100
 // * Use a function to print the messages
+fn print_message(gt_100:bool){
+    match gt_100{
+        true=>println!("its big"),
+        false=>println!("its small"),
+    }
+}
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+fn main() {
+    let value=100;
+    let is_gt_100=value>100;
+    print_message(is_gt_100);
+}
