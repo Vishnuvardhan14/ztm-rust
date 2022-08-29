ba948ea8bcbb6cf2cc4fb7de11ca2c6a37ec8591
Vishnuvardhan14 <vishnu14aug@gmail.com>
2022-08-29 21:28:33 +0530

    example of struct book

--- activities/src/bin/demo5.rs
+++ activities/src/bin/demo5.rs
@@ -1,6 +1,7 @@
 struct Book{
     pages: i32,
     rating: i32,
+    price: i32,
 }
 fn display_page_count(book:&Book){
     println!("pages={:?}",book.pages);
@@ -8,11 +9,16 @@ fn display_page_count(book:&Book){
 fn display_rating(book:&Book){
     println!("rating={:?}",book.rating);
 }
+fn display_rating(book:&Book){
+    println!("price={:?}",book.price);
+}
 fn main(){
     let book=Book{
         pages: 5,
         rating: 9,
+        price:2000,
     };
     display_page_count(&book);
     display_rating(&book);
+    display_price(&book);
 }
