cfb8dec663489b80385a8e49fc4951d4a185e162
Vishnuvardhan14 <vishnu14aug@gmail.com>
2022-08-31 21:16:29 +0530

    Implementing functionality with the impl keyword

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -9,4 +9,49 @@
 // * Use an enum for the box color
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
-fn main(){}
+enum Color{
+    Brown,
+    Red
+}
+impl Color{
+    fn print(&self){
+        match self{
+            Color::Brown => PRINTLN!("brown"),
+            Color::Red=>println!("red"),
+        }
+    }
+}
+
+Struct Dimensions{
+    width:f64,
+    height:f64,
+    depth:f64,
+}
+impl Dimensions{
+    fn print(&self){
+        println!("width:{:?}",self.width);
+        println!("height:{:?}",self.height);
+        println!("depth:{:?}",self.depth);
+    }
+}
+Struct ShippingBox{
+    color: Color,
+    weight: f64,
+    dimensions: Dimensions
+}
+
+impl ShippingBox{
+    fn new(weight:f64, color: Color, dimensions: Dimensions) -> Self{
+        Self{
+            weight,
+            color,
+            dimensions,
+        }
+    }
+    fn print(&self){
+        self.color.print();
+    }
+}
+fn main(){
+    
+}
